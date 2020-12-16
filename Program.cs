9aa4ef144673e837a8db3a9a77959641ccb79945
DEV <dev@info.local>
2020-12-16 10:52:45 -0300

    Ex2 finalizado

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Fixicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float discount1 = 0.02f;
+            float discount2 = 0.03f;
+            float discount3 = 0.05f;
+
+            Console.WriteLine("Bem vindo ao Mercadinho do Back, escreva o nome do produto que você comprou: ");
+            string produto = Console.ReadLine();
+
+            Console.WriteLine($"Agora escreva quantas unidades do(a) {produto} você comprou: ");
+            int unidades = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Agora escreva o valor unitário do(a) {produto}: ");
+            float preco = float.Parse(Console.ReadLine());
+
+            if(unidades <= 5){
+
+                float total = (unidades * preco);
+                float desconto = (total * discount1);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}");
+
+            }else if(unidades >5 || unidades <=10){
+
+                float total = (unidades * preco);
+                float desconto = (total * discount2);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}");
+                
+            }else{
+
+                float total = (unidades * preco);
+                float desconto = (total * discount3);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}"); 
+        
+            }
+        }
+    }
+}
